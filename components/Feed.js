14178cd553e864ceda30eb3bd262e09022a29f14
abdohassan <ahassan@synapse-analytics.io>
2023-10-01 17:47:35 +0300

    adding prompt card for user

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,59 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+import Loading from '@/app/loading';
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState('');
+  const [loadingPrompts, setLoadingPrompts] = useState(false);
+  const [prompts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    setLoadingPrompts(true);
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      if (response.status === 200) {
+        setLoadingPrompts(false);
+        setPosts(data);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      {loadingPrompts ? (
+        <Loading />
+      ) : (
+        <div className='mt-16 prompt_layout'>
+          {prompts?.map((prompt) => (
+            <PromptCard
+              key={prompt._id}
+              prompt={prompt}
+              handleTagClick={() => {}}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default Feed;
--- components/PromptCard.js
+++ components/PromptCard.js
@@ -1,7 +1,26 @@
+import Image from 'next/image';
 import React from 'react';
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete }) => {
+  return (
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+          <Image
+            src={prompt.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+
+          <div className='flex flex-col'>
+            <h3>{prompt.creator.username}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PromptCard;
